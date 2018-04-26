efd0a732ae6abb13f5f4e9fa163f70c99611d2be
dkoger44 <dbkr222@gmail.com>
2018-04-26 16:23:07 -0400

    adding auto_increment primary keys
    Created sequences to handle the increment of primary keys.

--- recipeDB.sql
+++ recipeDB.sql
@@ -3,6 +3,8 @@ drop table RecipeIngredients cascade constraints;
 drop table Recipe cascade constraints;
 drop table MealPlan cascade constraints;
 drop table MealPlanRecipe cascade constraints;
+drop sequence seq_recipe;
+drop sequence seq_mealplan;
 
 create table Ingredients (
 Name varchar2(20) primary key,
@@ -15,7 +17,7 @@ Sugar varchar2(20)
 );
 
 create table Recipe (
-RecipeID char(9) primary key,
+RecipeID int primary key,
 Category varchar2(20),
 Instruction varchar2(60),
 Name varchar2(20)
@@ -23,21 +25,31 @@ Name varchar2(20)
 
 create table RecipeIngredients (
 IngredientsName varchar2(20),
-RecipeID char(9),
+RecipeID int,
 Qty number,
 constraint ingred foreign key (IngredientsName) references Ingredients(Name),
 constraint recip foreign key (RecipeID) references Recipe(RecipeID)
 );
 /* check this for accuracy */
 create table MealPlan(
-MealID char(9) primary key,
+MealID int primary key,
 DayOfWeek varchar2(20) 
 );
 
 create table MealPlanRecipe(
-RecipeID char(9),
-MealID char(9),
+RecipeID int,
+MealID int,
 Meal varchar2(20),
 constraint  incRecipe foreign key (RecipeID) references Recipe(RecipeID),
 constraint incMeal foreign key (MealID) references MealPlan(MealID)
 );
+CREATE SEQUENCE seq_recipe
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+CREATE SEQUENCE seq_mealplan
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
